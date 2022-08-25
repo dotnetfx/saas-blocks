6204797723ad94ef8b1af87b6cd992541bc1de9b
Derek Rowley <derekrowley@dotnetfx.com>
2022-08-25 14:50:12 -0600

    latest

--- apps/web/babel.config.js
+++ apps/web/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    presets: ["@native-base/next-adapter/babel"],
-    plugins: [
-      ["@babel/plugin-proposal-private-methods", { loose: true }],
-      ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-    ],
-  };
+  presets: ["@native-base/next-adapter/babel"],
+  plugins: [
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }]
+  ],
+};
